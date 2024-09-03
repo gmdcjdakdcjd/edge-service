782456bf0e08d5ed8d734417af99751aaebf15ac
gmdcjdakdcjd <ysh010223@naver.com>
2024-09-03 17:59:19 +0900

    v4

--- src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
@@ -9,11 +9,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RateLimiterConfig {
 
-	@Bean
-	KeyResolver keyResolver() {
-		return exchange -> exchange.getPrincipal()
-				.map(Principal::getName)
-				.defaultIfEmpty("anonymous");
-	}
+    @Bean
+    KeyResolver keyResolver() {
+        return exchange -> exchange.getPrincipal()
+                .map(Principal::getName)
+                .defaultIfEmpty("anonymous");
+    }
 
 }
